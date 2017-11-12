11273fbdd7bc65a6cf8615d3c6fef8f916454e3b
Isaac W Hanson <isaac@starlig.ht>
2017-11-12 12:31:26 -0500

    project skeleton

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xlang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
